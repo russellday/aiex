c178869ee10f9430ed42ca95e0fb1067b7a8f615
Day <russeday@amazon.com>
2016-08-08 15:28:17 -0700

    updates

--- modules/ignite/rest-service.js
+++ modules/ignite/rest-service.js
@@ -3,11 +3,19 @@ var _ = require('underscore');
 var logger = global.logger;
 var request = require("request");
 var r = process.env.IGNITE_REST;
-var rootUrl = r !== undefined ? "http://" + process.env.IGNITE_REST + "/ignite?cmd=" : "http://localhost:8080/ignite?cmd=";
+//var rootUrl = r !== undefined ? "http://" + process.env.IGNITE_REST + "/ignite?cmd=" : "http://localhost:8080/ignite?cmd=";
+var rootUrl = "http://54.209.43.144/ignite?cmd="
+
 
 module.exports = function() {
     this.proxyCall = function(methodName, query){
         var deferred = q.defer();
+
+        console.log("rootUrl:", rootUrl);
+        console.log("query:", query);
+
+        //TODO: If method is TOP: S3 List, Iterate
+        //else update rootUrl per a dictionary or array
 
         request({
             method:"GET",
